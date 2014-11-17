fe774e3b95051ef0d6f5c157a736298ea1588b28
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-11-17 11:54:07 -0500

    more little things

--- iteration1.m
+++ iteration1.m
@@ -2,10 +2,13 @@ function res = iteration1(a1, a2, l1, l2)
     P1 = [l1*sin(a1), -l1*cos(a1)];
     % P2 = [l2*sin(a2), -l2*cos(a2)];
     n = 100;
-    P = zeros(n, 2);
+    P = zeros(n, 4);
     P(1, 1) = P1(1);
     P(1, 2) = P1(2);
-    for i = 1:n
-        % do stuff
+    % P(1, 3) = P2(1);
+    % P(1, 4) = P2(2);
+    for i = 2:n
+        P(i, 1) % = ??
+        P(i, 2) % = ??
     end
 end
