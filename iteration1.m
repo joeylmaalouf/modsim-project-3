51bb07adc6c2f368a7b7cdefffea6c916ac8e7a4
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-11-19 10:21:21 -0500

    working single pendulum

--- iteration1.m
+++ iteration1.m
@@ -1,9 +1,16 @@
-function res = iteration1(a1, a2, l1, l2)
+function res = iteration1(a1, l1)
     n = 200;
     g = -9.81;
-    % use ode45
-end
-
-function res = td(tdd, g, L)
-    res = cos(asin(-L/g*tdd))*2*g/L;
+    V = 0;
+    [T, O] = ode45(@calc, 0:0.01:100, [a1, V]);
+    plot(l1*sin(O(:, 1)), l1*cos(O(:, 1)));
+    res = [O(end, 1), O(end, 2)];
+    
+    function res = calc(t, W)
+        theta = W(1);  % angle of pendulum
+        omega = W(2);  % rotational velocity
+        dTHETAdt = omega;
+        dOMEGAdt = -g*sin(theta)/l1;
+        res = [dTHETAdt; dOMEGAdt];
+    end
 end
