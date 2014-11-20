5a6f2a7477591cea112cb88c0e3e279c6f09e1d0
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-11-20 11:56:13 -0500

    added iteration 4, began attempting animation

--- iteration4.m
+++ iteration4.m
@@ -1,19 +1,37 @@
-function res = iteration3(a1, l1, a2, l2, mratio)
+function res = iteration4(a1, l1, a2, l2, mratio)
     m1 = mratio; m2 = 1;
     n = 200; g = -9.81;
     v1 = 0; v2 = 0;
     [T, O] = ode45(@calc, 0:0.01:100, [a1*pi/180, a2*pi/180, v1, v2]);
-    hold on;
-    plot(l1*sin(O(:, 1)), l1*cos(O(:, 1)), 'r');  % calculate x and y coords from length and angle
-    plot(l1*sin(O(:, 1))+l2*sin(O(:, 2)), l1*cos(O(:, 1))+l2*cos(O(:, 2)), 'g');
     res = [O(end, 1), O(end, 2)];
-    
+    % calculate x and y coords from length and angle
+    coords = zeros(length(O), 4);
+    coords(:, 1) = l1*sin(O(:, 1));
+    coords(:, 2) = l1*cos(O(:, 1));
+    coords(:, 3) = l1*sin(O(:, 1))+l2*sin(O(:, 2));
+    coords(:, 4) = l1*cos(O(:, 1))+l2*cos(O(:, 2));
+    hold on;
+    plot(coords(:, 1), coords(:, 2), 'r');
+    plot(coords(:, 3), coords(:, 4), 'g');
+    ax = gca;
+    h = hgtransform('Parent', ax);
+    hold on;
+    plot(coords(1, 1), coords(1, 2), 'ro', 'Parent', h);
+    hold off;
+%    for i = 2:length(O)
+%        m = makehgtform('translate',...
+%            coords(i, 1)-coords(1, 1),...
+%            coords(i, 2)-coords(1, 2), 0);
+%        h.Matrix = m;
+%        drawnow;
+%    end
+
     function res = calc(t, W)
         theta1 = W(1);  % angle of pendulum 1
         theta2 = W(2);  % angle of pendulum 2
         omega1 = W(3);  % rotational velocity 1
         omega2 = W(4);  % rotational velocity 1
-        dTHETA1dt = omega1;
+        dTHETA1dt = omega1; %sqrt((2*(cos(theta1)-cos(a1)))/(m1*l1));
         dTHETA2dt = omega2;
         dOMEGA1dt = (-m2*l1*omega1^2*sin(theta1-theta2)...
             *cos(theta1-theta2)+g*m2*sin(theta2)*cos(theta1-theta2)...
