803475bf90ce3bf54c0e7bd4343ac48d3c79a42a
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-12-02 12:37:59 -0500

    removed old animation code

--- iteration8.py
+++ iteration8.py
@@ -1,4 +1,4 @@
-# http://matplotlib.org/examples/animation/double_pendulum_animated.html
+# thanks http://matplotlib.org/examples/animation/double_pendulum_animated.html
 
 from numpy import sin, cos, pi
 import numpy as np
@@ -41,31 +41,5 @@ y = integrate.odeint(derivs, state, t)
 
 x1 = l1 * sin(y[:, 0])
 y1 = -l1 * cos(y[:, 0])
-
 x2 = l2 * sin(y[:, 1]) + x1
 y2 = -l2 * cos(y[:, 1]) + y1
-
-fig = plt.figure()
-ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
-ax.grid()
-
-line, = ax.plot([], [], 'o-', lw=2)
-time_template = 'time = %.1fs'
-time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
-
-
-def init():
-    line.set_data([], [])
-    time_text.set_text('')
-    return line, time_text
-
-
-def animate(i):
-    thisx = [0, x1[i], x2[i]]
-    thisy = [0, y1[i], y2[i]]
-    line.set_data(thisx, thisy)
-    return line, time_text
-
-ani = animation.FuncAnimation(fig, animate, np.arange(1, len(y)),
-                              interval=30, blit=True, init_func=init)
-plt.show()
