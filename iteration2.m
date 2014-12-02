320d6da9fd1a9fb0c463a95e76676c7dbffc624a
Byron Wasti <byron.wasti@gmail.com>
2014-12-02 11:49:45 -0500

    Updated files

--- iteration2.m
+++ iteration2.m
@@ -6,8 +6,16 @@ function res = iteration2(a1, l1, a2, l2)
     [T, O] = ode45(@calc, 0:0.01:100, [a1*pi/180, a2*pi/180, v1, v2]);
     hold on;
     plot(l1*sin(O(:, 1)), l1*cos(O(:, 1)), 'r');
-    plot(l1*sin(O(:, 1))+l2*sin(O(:, 2)), l1*cos(O(:, 1))+l2*cos(O(:, 2)), 'g');
-    res = [O(end, 1), O(end, 2)];
+   
+    U = (l1*sin(O(:, 1) )+1) * abs(g) * 1;
+    K = O(:,2).^2 *1;
+    plot(O(:,2),'y')
+    %plot(U)
+    %plot(K,'r')
+    %plot(K+U,'k')
+    
+    %lin(O(:, 2)), l1*cos(O(:, 1))+l2*cos(O(:, 2)), 'g');
+    %res = [O(end, 1), O(end, 2)];
     
     function res = calc(t, W)
         theta1 = W(1);  % angle of pendulum 1
